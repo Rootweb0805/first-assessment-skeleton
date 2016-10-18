01af820101fd9195e49b46876ca9ce328ac1e065
Preston Leach <prestonsleach@gmail.com>
2016-10-18 13:38:16 -0500

    excepts host and port args

--- cli/src/cli.js
+++ cli/src/cli.js
@@ -7,16 +7,20 @@ export const cli = vorpal()
 
 let username
 let server
+let address
+let port
 
 cli
   .delimiter(cli.chalk['yellow']('ftd~$'))
 
 cli
-  .mode('connect <username>')
+  .mode('connect <address> <port> <username>')
   .delimiter(cli.chalk['green']('connected>'))
   .init(function (args, callback) {
+    address = args.address
+    port = args.port
     username = args.username
-    server = connect({ host: 'localhost', port: 8080 }, () => {
+    server = connect({ host: address, port: port }, () => {
       server.write(new Message({ username, command: 'connect' }).toJSON() + '\n')
       callback()
     })
@@ -30,13 +34,16 @@ cli
     })
   })
   .action(function (input, callback) {
-    const [ command, ...rest ] = words(input)
+    const [ command, ...rest ] = words(input, /[^, ]+/g)
     const contents = rest.join(' ')
 
     if (command === 'disconnect') {
       server.end(new Message({ username, command }).toJSON() + '\n')
     } else if (command === 'echo') {
       server.write(new Message({ username, command, contents }).toJSON() + '\n')
+// TO-DO
+//     } else if (command === '@<username>') {
+//       username.send(new Message({username, command, contents }) + '\n')
     } else {
       this.log(`Command <${command}> was not recognized`)
     }
