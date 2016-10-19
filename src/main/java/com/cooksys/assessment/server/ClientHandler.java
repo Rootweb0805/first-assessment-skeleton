0ac241a839fe146d604ece29f32dc76f8bcecfa6
Preston Leach <prestonsleach@gmail.com>
2016-10-19 18:32:49 -0500

    OMG I figured out the mapping and users works!!!!

--- src/main/java/com/cooksys/assessment/server/ClientHandler.java
+++ src/main/java/com/cooksys/assessment/server/ClientHandler.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.HashSet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,7 +17,17 @@ public class ClientHandler implements Runnable {
 	private Logger log = LoggerFactory.getLogger(ClientHandler.class);
 
 	private Socket socket;
-		
+
+	// private Users userObj() {
+	// Users users = new Users();
+	// //ConcurrentHashMap<String, Socket> userlist = new ConcurrentHashMap<>();
+	// users.setUserlist(Users.getUserlist());
+	// return users;
+	//
+	// }
+	//
+	// Users users = userObj();
+
 	public ClientHandler(Socket socket) {
 		super();
 		this.socket = socket;
@@ -32,40 +41,44 @@ public class ClientHandler implements Runnable {
 			PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
 
 			while (!socket.isClosed()) {
+				String userlist = Users.getUserlist().toString();
 				String raw = reader.readLine();
 				Message message = mapper.readValue(raw, Message.class);
-				Users obj = new Users();
-				
 
 				switch (message.getCommand()) {
-					case "connect":
-						log.info("user <{}> connected", message.getUsername());
-						obj.getUserlist().add(message.getUsername());
-						System.out.println(obj.getUserlist());
-						break;
-					case "disconnect":
-						log.info("user <{}> disconnected", message.getUsername());
-						this.socket.close();
-						break;
-					case "echo":
-						log.info("user <{}> echoed message <{}>", message.getUsername(), message.getContents());
-						String response = mapper.writeValueAsString(message);
-						writer.write(response);
-						writer.flush();
-						break;
-					case "@":
-						log.info("user <{}> direct message <{}>", message.getUsername(), message.getContents());
-						String direct = mapper.writeValueAsString(message);
-						writer.write(direct);
-						writer.flush();
-						break;
-					case "users":
-						log.info("user <{}> users request <{}>", message.getUsername(), message.getContents());
-						String userobj = mapper.writeValueAsString(obj);
-						writer.write(userobj);
-						writer.flush();
-						break;
-						
+				case "connect":
+					log.info("user <{}> connected", message.getUsername());
+					Users.getUserlist().put(message.getUsername(), socket);
+					break;
+				case "disconnect":
+					log.info("user <{}> disconnected", message.getUsername());
+					Users.getUserlist().remove(message.getUsername());
+					this.socket.close();
+					break;
+				case "echo":
+					log.info("user <{}> echoed message <{}>", message.getUsername(), message.getContents());
+					String response = mapper.writeValueAsString(message);
+					writer.write(response);
+					writer.flush();
+					System.out.println(response);
+					break;
+				case "users":
+					log.info("user <{}> users request <{}>", message.getUsername(), message.getContents());
+					Message usersMessage = new Message();
+					usersMessage.setUsername(message.getUsername());
+					usersMessage.setCommand("users");
+					usersMessage.setContents(userlist);
+					String UL = mapper.writeValueAsString(usersMessage);
+					writer.write(UL);
+					writer.flush();
+					break;
+				case "@":
+					log.info("user <{}> direct message <{}>", message.getUsername(), message.getContents());
+					String direct = mapper.writeValueAsString(message);
+					writer.write(direct);
+					writer.flush();
+					break;
+
 				}
 			}
 
--- src/main/java/com/cooksys/assessment/server/Users.java
+++ src/main/java/com/cooksys/assessment/server/Users.java
@@ -1,17 +1,32 @@
 package com.cooksys.assessment.server;
 
-import java.util.HashSet;
+import java.net.Socket;
+import java.util.Enumeration;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Users {
+	private static ConcurrentHashMap<String, Socket> userlist = new ConcurrentHashMap<>();
 
-	private HashSet<String> userlist = new HashSet<String>();
-
-	public HashSet<String> getUserlist() {
+	public static synchronized ConcurrentHashMap<String, Socket> getUserlist() {
 		return userlist;
 	}
 
-	public void setUserlist(HashSet<String> userlist) {
-		this.userlist = userlist;
+	public void setUserlist(ConcurrentHashMap<String, Socket> userlist) {
+		Users.userlist = userlist;
 	}
 
+	public static synchronized Enumeration<String> getUsers() {
+		return userlist.keys();
+	}
+	
 }
+
+// private static HashSet<String> userlist = new HashSet<String>();
+//
+// public static synchronized HashSet<String> getUserlist() {
+// return userlist;
+// }
+//
+// public void setUserlist(HashSet<String> userlist) {
+// Users.userlist = userlist;
+// }
